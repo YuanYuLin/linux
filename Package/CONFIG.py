19977ba9620ba53ea89922109bc7e724b5f59b1c
yyl <yuanyu.lin@gmail.com>
2020-03-31 16:02:25 +0800

    MAJOR_NUM=1 MINOR_NUM=14 AUX_NUM=0 update package.

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -14,6 +14,7 @@ build_arch = ""
 build_format = ""
 image_path = ""
 image_output_name = "linux_image"
+dtb_output_name = "linux_dtb"
 LINUX_VERSION = ""
 jobs_count = ""
 #LINUX_VERSION_FULL_STR="linux-4.3"
@@ -33,6 +34,7 @@ def set_global(args):
     global image_path
     global LINUX_VERSION
     global jobs_count
+    global dtb_path
     pkg_args = args["pkg_args"]
     def_cfg_version = "default_" + pkg_args["config"] + ".config"
     LINUX_VERSION = pkg_args["version"]
@@ -46,16 +48,25 @@ def set_global(args):
     tarball = ops.path_join(pkg_path, LINUX_VERSION_FULL_STR + ".tar.xz")
     build_dir = ops.path_join(output_dir, LINUX_VERSION_FULL_STR)
     src_def_config = ops.path_join(pkg_path, def_cfg_version)
+
+    image_dir = ""
     if arch == "armel":
+        image_dir = "arch/arm/boot/"
         if build_format == "uImage":
-            image_path = "arch/arm/boot/uImage"
+            image_path = image_dir + "uImage"
         else:
-            image_path = "arch/arm/boot/zImage"
+            image_path = image_dir + "zImage"
     elif arch == "x86_64":
+        image_dir = "arch/x86/boot/"
         build_arch = "x86"
-        image_path = "arch/x86/boot/bzImage"
+        image_path = image_dir + "bzImage"
     else:
         sys.exit(1)
+
+    dtb_path = ""
+    if "dtb" in pkg_args :
+        device_tree_binary = pkg_args["dtb"]
+        dtb_path = image_dir + "dts/" + device_tree_binary
 
     dst_def_config = ops.path_join(build_dir, ".config")
     initramfs_file = ops.path_join(iopc.getOutputRootDir(), initramfs_name)
@@ -129,6 +140,13 @@ def MAIN_INSTALL(args):
     set_global(args)
 
     ops.copyto(ops.path_join(build_dir, image_path), ops.path_join(iopc.getOutputRootDir(), image_output_name))
+
+    print dtb_path
+    print "ADAM"
+    if dtb_path != "" :
+        if ops.isExist(ops.path_join(build_dir, dtb_path)) :
+            ops.copyto(ops.path_join(build_dir, dtb_path), ops.path_join(iopc.getOutputRootDir(), dtb_output_name))
+
     ops.mkdir(build_module_dir)
     ops.touch(ops.path_join(build_module_dir, "modules.dep"))
     '''
